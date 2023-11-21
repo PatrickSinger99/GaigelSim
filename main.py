e73bc00041d8d22fc02ded32d86b739703a0f07f
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2023-11-21 12:44:26 +0100

    update + winner selection

--- main.py
+++ main.py
@@ -37,9 +37,12 @@ class Player:
         self.id = Player.player_id_count
         Player.player_id_count += 1
 
+    def get_num_cards(self):
+        return sum(1 for value in self.cards_hand.values() if value is not None)
+
     def get_action(self):
         # TODO TEMP
-        return random.randint(1, 6)
+        return random.randint(1, 5)
 
 
 class GaigelSim:
@@ -51,10 +54,15 @@ class GaigelSim:
         # Initialize game variables
         self.card_stack = Queue(maxsize=48)
         self.players = Queue(maxsize=players)
-        self.card_under_stack = None
-        self.trumpf = None
+        self.trump_suit = None  # trump card under stack
+        self.trump = None
         self.card_round_stack = []
+        self.card_placed_by = []  # Tracks which player placed which card in the card_round_stack
         self.current_player = None
+
+        # Game time tracking
+        self.current_round = 0
+        self.current_turn = 0  # The progress within one round (Depending on the number of players)
 
         # Create new deck
         for card_type in GaigelSim.card_types.keys():
@@ -69,21 +77,27 @@ class GaigelSim:
 
         # Initial actions
         self.shuffle_stack()
-        self.hand_out_cards()
         self.select_starting_player()
+        self.hand_out_cards()
+
 
     def __str__(self):
-        return_string = (f"[CARD STACK] ({self.card_stack.qsize()} cards) "
-                         f"{', '.join([card.val() for card in self.card_stack.queue])}")
+        return_string = f"{'='*30} Round {self.current_round} | Turn {self.current_turn} {'='*30}\n"
+        banner_width = len(return_string)
 
-        return_string += f"\n[CARD UNDER STACK] {self.card_under_stack.val()}"
-        return_string += f"\n[TRUMPF] {self.trumpf}"
-        return_string += f"\n[CURRENT ROUND STACK] {', '.join([card.val() for card in self.card_round_stack])}"
+        return_string += (f"[CARD STACK] ({self.card_stack.qsize()} cards) "
+                          f"{', '.join([card.val() for card in self.card_stack.queue])}")
+
+        return_string += f"\n[TRUMP SUIT] {self.trump_suit.val()}"
+        return_string += f" | [TRUMP] {self.trump}"
+        return_string += f" | [CURRENT ROUND STACK] {', '.join([card.val() for card in self.card_round_stack])}"
 
         for player in self.players.queue:
             return_string += (f"\n[PLAYER: {player.name}] {'(current turn) ' if player == self.current_player else ''}"
-                              f"({len(player.cards_hand)} cards, {player.points} points) "
-                              f"{', '.join([card.val() if card is not None else 'empty' for card in player.cards_hand.values()])}")  #
+                              f"({player.get_num_cards()} cards, {player.points} points) "
+                              f"{', '.join([card.val() if card is not None else 'empty' for card in player.cards_hand.values()])}")
+
+        return_string += "\n" + "="*(banner_width - 1)
 
         return return_string
 
@@ -93,32 +107,81 @@ class GaigelSim:
     def hand_out_cards(self):
         # Hand out first 3 cards for every player
         for i in range(1, 4):
-            for player in self.players.queue:
-                player.cards_hand[i] = self.card_stack.get()
+            for _ in range(self.players.qsize()):
+                self.draw_card_and_rotate()
 
-        # Define card under stack (Trumpf)
-        self.card_under_stack = self.card_stack.get()
-        self.trumpf = self.card_under_stack.type
+        # Define card under stack (trump suit)
+        self.trump_suit = self.card_stack.get()
+        self.trump = self.trump_suit.type
 
         # Hand out last 2 cards for every player
         for i in range(4, 6):
-            for player in self.players.queue:
-                player.cards_hand[i] = self.card_stack.get()
+            for _ in range(self.players.qsize()):
+                self.draw_card_and_rotate()
 
     def select_starting_player(self):
         self.current_player = random.choice(self.players.queue)
 
         # Rotate player queue to selected player
-        while self.players.queue[0] != self.current_player:
+        self.rotate_queue_to_player(self.current_player)
+
+        print(f"Selected starting player {self.current_player.name}")
+
+    def rotate_queue_to_player(self, player):
+        while self.players.queue[0] != player:
             rotated_player = self.players.get()
             self.players.put(rotated_player)
 
+    def draw_card(self, player):
+        empty_slot = list(player.cards_hand.keys())[list(player.cards_hand.values()).index(None)]
+        player.cards_hand[empty_slot] = self.card_stack.get()
+        print(f"{player.name} draws card {player.cards_hand[empty_slot].val()}")
+
+    def draw_card_and_rotate(self):
+        # Select next player and put them back in the queue
+        self.current_player = self.players.get()
+        self.players.put(self.current_player)
+
+        # Draw card from stack
+        self.draw_card(self.current_player)
+
+    def determine_winner(self):
+        start_type = self.card_round_stack[0].type
+        card_round_values = []
+
+        for card in self.card_round_stack:
+            card_value = card.value
+            if card.type == self.trump:
+                card_value += 1000
+            elif card.type == start_type:
+                card_value += 100
+
+            card_round_values.append(card_value)
+
+        winner = self.card_placed_by[card_round_values.index(max(card_round_values))]
+        print(f"{winner.name} wins the round")
+
+        return winner
+
+
     def play_round(self):
+
+        # Reset current turn count and increment round count
+        self.current_round += 1
+        self.current_turn = 0
+
+        # Reset turn variables
+        self.card_round_stack = []
+        self.card_placed_by = []
 
         # PART 1: Every player plays a card
         for _ in range(self.players.qsize()):
+            # Select next player and put them back in the queue
             self.current_player = self.players.get()
             self.players.put(self.current_player)
+
+            # Advance turn count
+            self.current_turn += 1
 
             player_action = self.current_player.get_action()
 
@@ -129,13 +192,15 @@ class GaigelSim:
                 # Add selected card to current round stack and remove from players hand
                 self.card_round_stack.append(self.current_player.cards_hand[player_action])
                 self.current_player.cards_hand[player_action] = None
+                self.card_placed_by.append(self.current_player)
 
-        # TODO select winner
+        # Select winner and rotate player queue to them
+        winner = self.determine_winner()
+        self.rotate_queue_to_player(winner)
 
         # PART 2: Every player draws a card starting at the winner
         for _ in range(self.players.qsize()):
-            # TODO
-            pass
+            self.draw_card_and_rotate()
 
 
 if __name__ == '__main__':
@@ -143,3 +208,4 @@ if __name__ == '__main__':
     print(sim)
     sim.play_round()
     print(sim)
+
